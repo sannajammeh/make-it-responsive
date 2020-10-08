6202b481dffe0e051d3b068f0c12b6b8965cfdeb
Mathias Kolberg <mathiaskolberg@MCK-MacBook-Pro.local>
2020-10-08 19:44:43 +0200

    Contact form

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function DataJS(element, array = false) {
   return document.querySelector(query);
 }
 
+
+// Responsive navbar
 const toggleCollapse = document.querySelector(".toggle-collapse");
 const navbarCollapse = document.querySelector(".navbar-collapse");
 const collapse = document.querySelector(".collapse");
@@ -11,3 +13,19 @@ const collapse = document.querySelector(".collapse");
 toggleCollapse.addEventListener("click", e => {
     navbarCollapse.classList.toggle("collapse");
 });
+
+// Add class active to nav links
+const sections = document.querySelectorAll('section');
+const navItems = document.querySelectorAll('.navItem');
+
+window.addEventListener('scroll', () => {
+    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - 86) - 1; 
+    if(current === sections.length) return;
+
+    navItems.forEach(navItem => {
+        navItem.classList.remove('active');
+    })
+    navItems[current].classList.add('active');
+});
+
+
